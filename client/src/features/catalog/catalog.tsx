1545f0fab454738f916ca5d452c0302d0d714f90
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2023-12-16 09:58:28 +0200

    Implemented UI for filtering and sorting

--- client/src/features/catalog/catalog.tsx
+++ client/src/features/catalog/catalog.tsx
@@ -2,26 +2,98 @@ import LoadingComponent from "../../app/layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
 import ProductList from "./ProductList";
 import { useEffect } from "react";
-import { fetchFilters, fetchProductsAsync, productSelectors } from "./catalogSlice";
+import {
+  fetchFilters,
+  fetchProductsAsync,
+  productSelectors,
+  setProductParams,
+} from "./catalogSlice";
+import {
+  Box,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  Grid,
+  Pagination,
+  Paper,
+  Typography,
+} from "@mui/material";
+import ProductSearch from "./ProductSearch";
+import RadioButtonGroup from "../../app/components/RadioButtonGroup";
+
+const sortOptions = [
+  { value: "name", label: "Alphabetical" },
+  { value: "priceDesc", label: "Price - High to low" },
+  { value: "price", label: "Price - Low to high" },
+];
 
 export default function Catalog() {
   const products = useAppSelector(productSelectors.selectAll);
-  const {productsLoaded, status, filtersLoaded} = useAppSelector(state => state.catalog);
+  const { productsLoaded, status, filtersLoaded, brands, types,productParams } =
+    useAppSelector((state) => state.catalog);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     if (!productsLoaded) dispatch(fetchProductsAsync());
-  }, [productsLoaded,dispatch]);
+  }, [productsLoaded, dispatch]);
 
   useEffect(() => {
     if (!filtersLoaded) dispatch(fetchFilters());
   }, [dispatch, filtersLoaded]);
 
-  if (status.includes("pending")) return <LoadingComponent message="Loading Products..." />
-  
+  if (status.includes("pending"))
+    return <LoadingComponent message="Loading Products..." />;
+
   return (
-    <>
-      <ProductList products={products}/>
-    </>
-  )
+    <Grid container spacing={4}>
+      <Grid item xs={3}>
+        <Paper sx={{ mb: 2 }}>
+          <ProductSearch />
+        </Paper>
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <RadioButtonGroup 
+            selectedValue={productParams.orderBy}
+            options={sortOptions}
+            onChange={(e) => dispatch(setProductParams({orderBy: e.target.value}))}
+          />
+        </Paper>
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <FormGroup>
+            {brands.map((brands) => (
+              <FormControlLabel
+                control={<Checkbox />}
+                label={brands}
+                key={brands}
+              />
+            ))}
+          </FormGroup>
+        </Paper>
+        <Paper sx={{ mb: 2, p: 2 }}>
+          <FormGroup>
+            {types.map((types) => (
+              <FormControlLabel
+                control={<Checkbox />}
+                label={types}
+                key={types}
+              />
+            ))}
+          </FormGroup>
+        </Paper>
+      </Grid>
+      <Grid item xs={9}>
+        <ProductList products={products} />
+      </Grid>
+      <Grid item xs={3}></Grid>
+      <Grid item xs={9}>
+        <Box
+          display={"flex"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+        >
+          <Typography>Displaying 1-6 of 20 items</Typography>
+          <Pagination color="secondary" size="large" count={10} page={2} />
+        </Box>
+      </Grid>
+    </Grid>
+  );
 }
